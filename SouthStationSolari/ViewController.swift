93823ea71bc75e836f233be50d1ba91d00f86bc8
Peter Eddy <peter.eddy@gmail.com>
2022-12-20 12:09:51 -0500

    Initial Commit

--- SouthStationSolari/ViewController.swift
+++ SouthStationSolari/ViewController.swift
@@ -6,14 +6,71 @@
 //
 
 import UIKit
+import Splitflap
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SplitflapDataSource, SplitflapDelegate {
+  
+  @IBOutlet weak var splitflap: Splitflap!
+  @IBOutlet weak var actionButton: UIButton!
+
+  private let words        = ["Hey you", "Bonsoir", "12h15", "Arrival"]
+  private var currentIndex = 0
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    splitflap.datasource = self
+    splitflap.delegate   = self
+    splitflap.reload()
   }
 
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
 
+    updateSplitFlapAction(actionButton)
+  }
+
+  // MARK: - Action Methods
+
+  @IBAction func updateSplitFlapAction(_ sender: AnyObject) {
+    splitflap.setText(words[currentIndex], animated: true, completionBlock: {
+      print("Display finished!")
+    })
+
+    currentIndex = (currentIndex + 1) % words.count
+
+    updateButtonWithTitle(words[currentIndex])
+  }
+
+  private func updateButtonWithTitle(_ title: String) {
+    actionButton.setTitle("Say \(words[currentIndex])!", for: .normal)
+  }
+
+  // MARK: - Splitflap DataSource Methods
+
+  func numberOfFlapsInSplitflap(_ splitflap: Splitflap) -> Int {
+    return 7
+  }
+
+  func tokensInSplitflap(_ splitflap: Splitflap) -> [String] {
+    return SplitflapTokens.AlphanumericAndSpace
+  }
+
+  // MARK: - Splitflap Delegate Methods
+
+  func splitflap(splitflap: Splitflap, rotationDurationForFlapAtIndex index: Int) -> Double {
+    return 0.2
+  }
+
+  func splitflap(splitflap: Splitflap, builderForFlapAtIndex index: Int) -> FlapViewBuilder {
+    return FlapViewBuilder { builder in
+      builder.backgroundColor = UIColor.black
+      builder.cornerRadius    = 5
+      builder.font            = UIFont(name: "Courier", size: 80)
+      builder.textAlignment   = .center
+      builder.textColor       = UIColor.white
+      builder.lineColor       = UIColor.darkGray
+    }
+  }
 }
 
